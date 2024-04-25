9d1e3804fc6f73b3365d8f7d7c8c4747d1d13ef7
kawin35k <kawin35k@gmail.com>
2024-04-25 13:10:20 -0300

    arreglos en modulo crea_equipo. Ahora agregar mostrar persona correctamente en el metodo  dame equipo

--- crea_equipo.py
+++ crea_equipo.py
@@ -1,7 +1,7 @@
 from clase_persona import persona
 import numpy as np
 
-class CreaEquipo:
+class Equipo:
     __listaPersonas=None
     __lider=int
 
@@ -15,22 +15,29 @@ class CreaEquipo:
         if(len(xpostulantes)<len(self.__listaPersonas)):
             print("LA LISTA NO TIENE LA CANTIDAD DE INTEGRANTES MINIMA...")
         else:
-            equipo_habilidad=np.array()
+            equipo_habilidad=None
+            habilidades_postulantes=[]
             indice_lider=None
-            contador=0
+            contador=1#empieza en uno por que el indice 0 ya esta ocupado por el lider
             j=0
             for i in xpostulantes:
-                equipo_habilidad=np.concatenate([i.dame_area(xarea)])
+                habilidades_postulantes.append(i.dame_area(xarea))#guarda las habilidades
+            #for i in listaDePersonas:
+            #   a.append(i.dame_area(2))
+            equipo_habilidad=np.array(habilidades_postulantes)#casteo o refundicion
             indice_lider=np.where(equipo_habilidad.max())#busca el mayor valor y el indice al que pertenece
-            self.__lider=indice_lider[0][0]
-            xpostulantes[indice_lider].actualizar_ocupado(True)
-            self.__listaPersonas[0]=xpostulantes[indice_lider]
-            while((contador<len(self.__listaPersonas))and j<len(xpostulantes)):
+            self.__lider=indice_lider[0][0]#para que en caso de haber mas de un indice solo se quede con el primero
+            xpostulantes[self.__lider].actualizar_ocupado(True)
+            self.__listaPersonas[0]=xpostulantes[self.__lider]
+            #el lider del equipo es el primer elemento del arreglo
+            while((contador<len(self.__listaPersonas))and j<len(xpostulantes)):#itera hasta que completa el equipo(litsaPersonas) o terminar con los postulantes
                 if ((xpostulantes[j].dame_ocupado()==False)and(xpostulantes[j].dame_area(xarea)<=equipo_habilidad.max()/1.5)and(xpostulantes[j].dame_area(xarea)>equipo_habilidad.max()/4)):
+                #este if pone como condiciones que la persona este libre y que cumpla con un rango de hanilidad, debe ser menor que el lider para asi aprender de el
                     self.__listaPersonas[contador]=xpostulantes[j]
                     xpostulantes[j].actualizar_ocupado(True)
                     contador+=1
                 j+=1
             #SELECCIONAR LIDER, quiza use la funcion .dame_especialidad
-
+    def dame_equipo(self):
+        return self.__listaPersonas
 
--- pruebas_NP.py
+++ pruebas_NP.py
@@ -1,7 +1,42 @@
 import numpy as np
-a=2
-lista=[1,5,4,3,2,7,8,9,9,5]
-arr=np.array(lista)
-brr=np.empty(a,dtype=int)
-b=np.concatenate((arr,[100]),axis=0)
-print(b)
+from clase_persona import persona
+from crea_equipo import Equipo
+
+equipo1=Equipo(3)
+
+per1=persona()
+per1.carga_persona("A","1")
+#print(per.mostrar_datos())
+per1.ingresa_habilidad(2,4)
+#print(per1.dame_habilidades().max()) FUNCIONA
+per2=persona()
+per2.carga_persona("B","2")
+per2.ingresa_habilidad(2,3)
+per3=persona()
+per3.carga_persona("C","3")
+#print(per.mostrar_datos())
+per3.ingresa_habilidad(2,5)
+per4=persona()
+per4.carga_persona("D","4")
+per4.ingresa_habilidad(2,8)
+per5=persona()
+per5.carga_persona("E","5")
+per5.ingresa_habilidad(2,10)
+
+listaDePersonas=[per1,per2,per3,per4,per5]
+a=[]
+#for i in listaDePersonas:   #este for es el de la linea 23 del comulo crea_equipo
+ #   a.append(i.dame_area(2))#concatena(agrega) elementos al arreglo
+#print(a)
+equipo1.crea_automatico(listaDePersonas,2)
+print(equipo1.dame_equipo())
+
+#a=2
+#perslita=[per1,per2]
+#lista=[1,5000,4,3,2,7,8,9,9,5]
+#arr=np.array(lista)
+#brr=np.empty(a,dtype=int)
+#b=np.array(perslita)
+#b=np.concatenate((arr,[100]),axis=0)   de esta forma se puede concatenar arreglos
+#print(b)
+#print(listaArea.max())
